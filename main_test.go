8e7e47f2b96a8d72f4755f164d1ec8dd41a2c25a
Iori Mizutani <iori.mizutani@gmail.com>
2017-12-17 15:52:30 +0900

    add some tests on main

--- main_test.go
+++ main_test.go
@@ -6,6 +6,7 @@
 package main
 
 import (
+	"os"
 	"reflect"
 	"testing"
 
@@ -39,12 +40,132 @@ func Test_loadFiltersFromCSVFile(t *testing.T) {
 		args args
 		want filtering.Subscriptions
 	}{
-	// TODO: Add test cases.
+		{"SGTIN-96_3_3_458960468_102",
+			args{getPackagePath() + "/testdata/filters.csv"},
+			filtering.Subscriptions{
+				"0011000001101101101101011011001011100101010000000001100110": &filtering.Info{"SGTIN-96_3_3_458960468_102", 0},
+			}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := loadFiltersFromCSVFile(tt.args.f); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("loadFiltersFromCSVFile() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_getPackagePath(t *testing.T) {
+	tests := []struct {
+		name string
+		want string
+	}{
+		{"os.Getenv(\"GOPATH\") + \"/src/github.com/iomz/gosstrak-fc\"", os.Getenv("GOPATH") + "/src/github.com/iomz/gosstrak-fc"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := getPackagePath(); got != tt.want {
+				t.Errorf("getPackagePath() = \n%v, want \n%v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_runAnalyzePatricia(t *testing.T) {
+	type args struct {
+		head    *filtering.PatriciaTrie
+		inFile  string
+		outFile string
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			runAnalyzePatricia(tt.args.head, tt.args.inFile, tt.args.outFile)
+		})
+	}
+}
+
+func Test_runDumb(t *testing.T) {
+	type args struct {
+		idFile string
+		sub    filtering.Subscriptions
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			runDumb(tt.args.idFile, tt.args.sub)
+		})
+	}
+}
+
+func Test_execute(t *testing.T) {
+	type args struct {
+		idFile  string
+		head    filtering.Engine
+		outFile string
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			execute(tt.args.idFile, tt.args.head, tt.args.outFile)
+		})
+	}
+}
+
+func Test_loadHuffmanTree(t *testing.T) {
+	type args struct {
+		filterFile   string
+		engineFile   string
+		isRebuilding bool
+	}
+	tests := []struct {
+		name string
+		args args
+		want *filtering.HuffmanTree
+	}{
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := loadHuffmanTree(tt.args.filterFile, tt.args.engineFile, tt.args.isRebuilding); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("loadHuffmanTree() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_loadPatriciaTrie(t *testing.T) {
+	type args struct {
+		filterFile   string
+		engineFile   string
+		isRebuilding bool
+	}
+	tests := []struct {
+		name string
+		args args
+		want *filtering.PatriciaTrie
+	}{
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := loadPatriciaTrie(tt.args.filterFile, tt.args.engineFile, tt.args.isRebuilding); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("loadPatriciaTrie() = %v, want %v", got, tt.want)
 			}
 		})
 	}
