7533d9e8afce3d7ee1e1ea39866fbd672fe84510
Iori Mizutani <iori.mizutani@gmail.com>
2018-07-05 22:19:26 +0900

    add (un)marshaller for Subscriptions

--- filtering/subscriptions.go
+++ filtering/subscriptions.go
@@ -7,9 +7,13 @@ package filtering
 
 import (
 	"bytes"
+	"encoding/csv"
+	"encoding/gob"
 	"fmt"
 	"io"
+	"os"
 	"sort"
+	"strconv"
 	"strings"
 )
 
@@ -22,6 +26,53 @@ type Info struct {
 	NotificationURI string
 	EntropyValue    float64
 	Subset          Subscriptions
+}
+
+// MarshalBinary overwrites the marshaller in gob encoding *Info
+func (info *Info) MarshalBinary() (_ []byte, err error) {
+	var buf bytes.Buffer
+	enc := gob.NewEncoder(&buf)
+
+	// Offset
+	enc.Encode(info.Offset)
+
+	// NotificationURI
+	enc.Encode(info.NotificationURI)
+
+	// EntropyValue
+	enc.Encode(info.EntropyValue)
+
+	// Subset
+	enc.Encode(info.Subset)
+
+	//buf.Encode
+	return buf.Bytes(), err
+}
+
+// UnmarshalBinary overwrites the unmarshaller in gob decoding *Info
+func (info *Info) UnmarshalBinary(data []byte) (err error) {
+	dec := gob.NewDecoder(bytes.NewReader(data))
+
+	// Offset
+	if err = dec.Decode(&info.Offset); err != nil {
+		return
+	}
+
+	// NotificationURI
+	if err = dec.Decode(&info.NotificationURI); err != nil {
+		return
+	}
+
+	// EntropyValue
+	if err = dec.Decode(&info.EntropyValue); err != nil {
+		return
+	}
+	// Subset
+	if err = dec.Decode(&info.Subset); err != nil {
+		return
+	}
+
+	return
 }
 
 // Clone retuns a new copy of subscriptions
@@ -95,6 +146,101 @@ func (sub Subscriptions) linkSubset() {
 		}
 	}
 	return
+}
+
+// MarshalBinary overwrites the marshaller in gob encoding *Subscriptions
+func (sub *Subscriptions) MarshalBinary() (_ []byte, err error) {
+	var buf bytes.Buffer
+	enc := gob.NewEncoder(&buf)
+
+	// Size of Subscriptions
+	enc.Encode(len(*sub))
+	for _, fs := range (*sub).Keys() {
+		// FilterString
+		enc.Encode(fs)
+		// Info
+		err = enc.Encode((*sub)[fs])
+	}
+
+	return buf.Bytes(), err
+}
+
+// UnmarshalBinary overwrites the unmarshaller in gob decoding List
+func (sub *Subscriptions) UnmarshalBinary(data []byte) (err error) {
+	dec := gob.NewDecoder(bytes.NewReader(data))
+
+	// Size of List
+	var subSize int
+	if err = dec.Decode(&subSize); err != nil {
+		return
+	}
+
+	for i := 0; i < subSize; i++ {
+		// FilterString
+		var fs string
+		if err = dec.Decode(&fs); err != nil {
+			return
+		}
+
+		/// Info
+		info := Info{
+			Subset: Subscriptions{},
+		}
+		err = dec.Decode(&info)
+
+		// add to the sub
+		(*sub)[fs] = &info
+	}
+
+	return
+}
+
+// LoadFiltersFromCSVFile takes a csv file name and generate Subscriptions
+func LoadFiltersFromCSVFile(f string) Subscriptions {
+	sub := Subscriptions{}
+	fp, err := os.Open(f)
+	if err != nil {
+		panic(err)
+	}
+	defer fp.Close()
+
+	reader := csv.NewReader(fp)
+	reader.Comma = ','
+	reader.LazyQuotes = true
+	for {
+		record, err := reader.Read()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			panic(err)
+		}
+		if len(record) < 3 {
+			// Default case
+			// prefix as key, *filtering.Info as value
+			sub[record[1]] = &Info{
+				Offset:          0,
+				NotificationURI: record[0],
+				EntropyValue:    0,
+				Subset:          Subscriptions{},
+			}
+		} else {
+			// For OptimalBST, filter with EntropyValue
+			// prefix as key, *filtering.Info as value
+			pValue, err := strconv.ParseFloat(record[2], 64)
+			if err != nil {
+				panic(err)
+			}
+			fs := record[1]
+			uri := record[0]
+			sub[fs] = &Info{
+				Offset:          0,
+				NotificationURI: uri,
+				EntropyValue:    pValue,
+				Subset:          Subscriptions{},
+			}
+		}
+	}
+	return sub
 }
 
 func recalculateEntropyValue(sub Subscriptions) float64 {
--- filtering/subscriptions_test.go
+++ filtering/subscriptions_test.go
@@ -156,3 +156,163 @@ func Test_recalculateEntropyValue(t *testing.T) {
 		})
 	}
 }
+
+func TestInfo_MarshalBinary(t *testing.T) {
+	type fields struct {
+		Offset          int
+		NotificationURI string
+		EntropyValue    float64
+		Subset          Subscriptions
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		want    []byte
+		wantErr bool
+	}{
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			info := &Info{
+				Offset:          tt.fields.Offset,
+				NotificationURI: tt.fields.NotificationURI,
+				EntropyValue:    tt.fields.EntropyValue,
+				Subset:          tt.fields.Subset,
+			}
+			got, err := info.MarshalBinary()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Info.MarshalBinary() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Info.MarshalBinary() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestInfo_UnmarshalBinary(t *testing.T) {
+	type fields struct {
+		Offset          int
+		NotificationURI string
+		EntropyValue    float64
+		Subset          Subscriptions
+	}
+	type args struct {
+		data []byte
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			info := &Info{
+				Offset:          tt.fields.Offset,
+				NotificationURI: tt.fields.NotificationURI,
+				EntropyValue:    tt.fields.EntropyValue,
+				Subset:          tt.fields.Subset,
+			}
+			if err := info.UnmarshalBinary(tt.args.data); (err != nil) != tt.wantErr {
+				t.Errorf("Info.UnmarshalBinary() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestSubscriptions_Clone(t *testing.T) {
+	tests := []struct {
+		name string
+		sub  Subscriptions
+		want Subscriptions
+	}{
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.sub.Clone(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Subscriptions.Clone() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestSubscriptions_Keys(t *testing.T) {
+	tests := []struct {
+		name string
+		sub  Subscriptions
+		want []string
+	}{
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.sub.Keys(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Subscriptions.Keys() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestSubscriptions_MarshalBinary(t *testing.T) {
+	tests := []struct {
+		name    string
+		sub     *Subscriptions
+		want    []byte
+		wantErr bool
+	}{
+		{
+			"nil marshal sub",
+			&Subscriptions{},
+			[]byte{3, 4, 0, 0},
+			false,
+		},
+		{
+			"simple marshal sub",
+			&Subscriptions{
+				"010101": &Info{Offset: 0, NotificationURI: "hoge", EntropyValue: 0.3, Subset: Subscriptions{}},
+				"1010": &Info{Offset: 0, NotificationURI: "foo", EntropyValue: 0.7, Subset: Subscriptions{
+					"11": &Info{Offset: 4, NotificationURI: "bar", EntropyValue: 0.2, Subset: Subscriptions{}}}},
+			},
+			[]byte{3, 4, 0, 4, 9, 12, 0, 6, 48, 49, 48, 49, 48, 49, 10, 255, 139, 6, 1, 2, 255, 142, 0, 0, 0, 25, 255, 143, 6, 1, 1, 13, 83, 117, 98, 115, 99, 114, 105, 112, 116, 105, 111, 110, 115, 1, 255, 144, 0, 0, 0, 65, 255, 140, 0, 61, 3, 4, 0, 0, 7, 12, 0, 4, 104, 111, 103, 101, 11, 8, 0, 248, 51, 51, 51, 51, 51, 51, 211, 63, 25, 255, 143, 6, 1, 1, 13, 83, 117, 98, 115, 99, 114, 105, 112, 116, 105, 111, 110, 115, 1, 255, 144, 0, 0, 0, 10, 255, 139, 6, 1, 2, 255, 142, 0, 0, 0, 7, 12, 0, 4, 49, 48, 49, 48, 64, 255, 140, 0, 60, 3, 4, 0, 0, 6, 12, 0, 3, 102, 111, 111, 11, 8, 0, 248, 102, 102, 102, 102, 102, 102, 230, 63, 25, 255, 143, 6, 1, 1, 13, 83, 117, 98, 115, 99, 114, 105, 112, 116, 105, 111, 110, 115, 1, 255, 144, 0, 0, 0, 10, 255, 139, 6, 1, 2, 255, 142, 0, 0, 0},
+			false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := tt.sub.MarshalBinary()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Subscriptions.MarshalBinary() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Subscriptions.MarshalBinary() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestLoadFiltersFromCSVFile(t *testing.T) {
+	type args struct {
+		f string
+	}
+	tests := []struct {
+		name string
+		args args
+		want Subscriptions
+	}{
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := LoadFiltersFromCSVFile(tt.args.f); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("LoadFiltersFromCSVFile() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
