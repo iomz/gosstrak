ddd20efee08fa7038ae890766e484251d2b58f14
Iori Mizutani <iori.mizutani@gmail.com>
2018-07-05 22:14:46 +0900

    add gobsubs cmd

---
+++ cmd/gobsubs/main_test.go
@@ -0,0 +1,11 @@
+// Copyright (c) 2018 Iori Mizutani
+//
+// Use of this source code is governed by The MIT License
+// that can be found in the LICENSE file.
+
+package main
+
+import "testing"
+
+func Test_main(t *testing.T) {
+}
