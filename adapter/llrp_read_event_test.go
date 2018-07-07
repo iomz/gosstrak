d217f2dfdc24c22eacbbb3dab61ccf6ed258cde0
Iori Mizutani <iori.mizutani@gmail.com>
2018-07-07 11:13:01 +0900

    rewrite BenchmarkUnmarshalROAccessReportBody with b.N

--- adapter/llrp_read_event_test.go
+++ adapter/llrp_read_event_test.go
@@ -40,7 +40,7 @@ func TestUnmarshalROAccessReportBody(t *testing.T) {
 	pdu := int(1500)
 	trds := limitedTags.BuildTagReportDataStack(pdu)
 	if len(trds) == 0 {
-		t.Fatal("TagReportDataStack generation was failed")
+		t.Fatal("TagReportDataStack generation failed")
 	}
 
 	var res []*LLRPReadEvent
@@ -54,40 +54,61 @@ func TestUnmarshalROAccessReportBody(t *testing.T) {
 	}
 }
 
-func benchmarkUnmarshalROAccessReportBody(size int, b *testing.B) {
+func BenchmarkUnmarshalROAccessReportBody(b *testing.B) {
 	largeTagsGOB := os.Getenv("GOPATH") + "/src/github.com/iomz/gosstrak/testdata/million-tags.gob"
 	// load up the tags from the file
 	var largeTags golemu.Tags
 	binutil.Load(largeTagsGOB, &largeTags)
 
+	cycle := b.N / len(largeTags)
+	remaining := b.N % len(largeTags)
+
 	// cap the tags with the given size
 	var limitedTags golemu.Tags
 	perms := rand.Perm(len(largeTags))
 	for n, i := range perms {
-		if n < size {
+		if n < remaining {
 			limitedTags = append(limitedTags, largeTags[i])
 		} else {
 			break
 		}
-		if n+1 == len(largeTags) {
-			b.Fatal("given tag size is larger than the testdata available")
+		if n == len(largeTags) {
+			b.Skip("given tag size is larger than the testdata available")
 		}
 	}
 
 	// build ROAR message
 	pdu := int(1500)
-	trds := limitedTags.BuildTagReportDataStack(pdu)
+	trds := largeTags.BuildTagReportDataStack(pdu)
 	if len(trds) == 0 {
 		b.Fatal("TagReportDataStack generation was failed")
 	}
+	limitedTRDs := limitedTags.BuildTagReportDataStack(pdu)
+	if len(limitedTRDs) == 0 && remaining != 0 {
+		b.Logf("len(limitedTags): %v, len(limitedTRDs: %v", len(limitedTags), len(limitedTRDs))
+		b.Logf("b.N: %v, cycle: %v, remaining: %v", b.N, cycle, remaining)
+		b.Fatal("TagReportDataStack generation failed")
+	}
 
+	var res []*LLRPReadEvent
 	b.ResetTimer()
-	for i, trd := range trds {
+	for c := 0; c < cycle; c++ {
+		for i, trd := range trds {
+			b.StopTimer()
+			roar := llrp.ROAccessReport(trd.Parameter, uint32(i))
+			b.StartTimer()
+			res = append(res, UnmarshalROAccessReportBody(roar[10:])...)
+		}
+	}
+
+	for i, trd := range limitedTRDs {
 		b.StopTimer()
 		roar := llrp.ROAccessReport(trd.Parameter, uint32(i))
 		b.StartTimer()
-		_ = UnmarshalROAccessReportBody(roar[10:])
+		res = append(res, UnmarshalROAccessReportBody(roar[10:])...)
+	}
+	b.StopTimer()
+	if b.N != len(res) {
+		b.Fatal("LLRP unmarshaller failed")
 	}
 }
-
-func BenchmarkUnmarshalROAccessReportBodyN(b *testing.B) { benchmarkUnmarshalROAccessReportBody(b.N, b) }
