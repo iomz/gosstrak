521803cd9d560b30018df4ac0b9a95afab4e492d
Iori Mizutani <iori.mizutani@gmail.com>
2019-09-02 11:43:19 +0200

    Change influxdb Go client repository

--- monitoring/stat_manager.go
+++ monitoring/stat_manager.go
@@ -9,7 +9,8 @@ import (
 	"log"
 	"time"
 
-	"github.com/influxdata/influxdb/client/v2"
+	_ "github.com/influxdata/influxdb1-client" // this is important because of the bug in go mod
+	client "github.com/influxdata/influxdb1-client/v2"
 )
 
 // StatManager receives stat and publish them to InfluxDB
